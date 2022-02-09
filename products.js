4a695da94c53f052cf75aea4242f139201c08d2d
Hon Sang Kong <hk1628@hotmail.com>
2022-02-10 09:06:16 +1100

    complete

---
+++ products.js
@@ -0,0 +1,19 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    img : "samsung j2 pro image.jpg",
+    name : "Samsung J2 Pro",
+    price : 100
+  },
+  124: {
+    img : "dummy-pdt-b.jpg",
+    name : "HP Notebook",
+    price : 299
+  },
+  125: {
+    img : "PANASONIC T44 LITE.jpg",
+    name : "Panasonic T44 Lite",
+    price : 125
+  },
+  
+};
